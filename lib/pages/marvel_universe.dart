1a3491d35ee0769852fbebc8f55ac75e0ccf4040
Vitor <sblvitor@gmail.com>
2023-04-11 20:30:15 -0400

    refactor: bottom navigation bar

--- lib/pages/marvel_universe.dart
+++ lib/pages/marvel_universe.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:marvel_universe/utils/colors.dart';
+import 'characters_page.dart';
+import 'comics_page.dart';
+import 'search_page.dart';
 
-import 'bottom_navigation_page.dart';
-
-class MarvelUniverse extends StatelessWidget {
+class MarvelUniverse extends StatefulWidget {
   const MarvelUniverse({super.key});
+
+  @override
+  State<MarvelUniverse> createState() => _MarvelUniverseState();
+}
+
+class _MarvelUniverseState extends State<MarvelUniverse> {
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +22,37 @@ class MarvelUniverse extends StatelessWidget {
         primarySwatch: primary,
         scaffoldBackgroundColor: const Color.fromARGB(255, 32, 32, 32)
       ),
-      home: const BottomNavigation(),
+      home: Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: const Color.fromARGB(255, 32, 32, 32),
+          unselectedItemColor: Colors.white60,
+          currentIndex: _currentIndex,
+          onTap: (value) {
+            // Respond to item press.
+            setState(() => _currentIndex = value);
+          },
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: "Characters"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                label: "Search"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.menu_book),
+                label: "Comics")
+          ],
+        ),
+          body: IndexedStack(
+            index: _currentIndex,
+            children: const <Widget>[
+              CharactersPage(title: 'Marvel Characters'),
+              SearchPage(),
+              ComicsPage(title: "Comics",),
+            ],
+          ),
+      ),
     );
   }
 }
